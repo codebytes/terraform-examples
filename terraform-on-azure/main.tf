2747a5c734549e5cb07824c45a35da96500ca630
Chris Ayers <clayers@gmail.com>
2022-10-10 01:35:04 -0400

    added storage

--- terraform-on-azure/main.tf
+++ terraform-on-azure/main.tf
@@ -3,3 +3,15 @@ resource "azurerm_resource_group" "state-demo-secure" {
   name     = "state-demo-${var.env}"
   location = var.location
 }
+
+resource "azurerm_storage_account" "example" {
+  name                     = "storageaccountname"
+  resource_group_name      = azurerm_resource_group.state-demo-secure.name
+  location                 = azurerm_resource_group.state-demo-secure.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = var.env
+  }
+}
