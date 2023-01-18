07dae74ce7737f530cf160d393d301d229618694
Chris Ayers <clayers@gmail.com>
2023-01-18 22:57:58 +0000

    demo

--- terraform-on-azure/modules/storage/main.tf
+++ terraform-on-azure/modules/storage/main.tf
@@ -1,17 +1,12 @@
 
-resource "azurerm_resource_group" "state-demo-secure" {
-  name     = "state-demo-${var.env}"
-  location = var.location
-}
-
 resource "azurerm_storage_account" "state-demo" {
-  name                     = "statedemocayers${var.env}"
+  name                     = "statedemocayers${var.environment}"
   resource_group_name      = azurerm_resource_group.state-demo-secure.name
   location                 = azurerm_resource_group.state-demo-secure.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
   tags = {
-    environment = var.env
+    environment = var.environment
   }
 }
---
+++ terraform-on-azure/modules/storage/variables.tf
@@ -0,0 +1,10 @@
+variable "name" {
+  type = string
+  description = "name of resource"
+}
+
+variable "location" {
+  type = string
+  description = "Location of resources"
+  default = "eastus"
+}
