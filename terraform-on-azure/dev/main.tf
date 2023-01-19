b6797fd28181070529de3ce818e318390e58be50
Chris Ayers <clayers@gmail.com>
2023-01-19 07:23:39 +0000

    lock

--- terraform-on-azure/dev/main.tf
+++ terraform-on-azure/dev/main.tf
@@ -18,4 +18,12 @@ provider "azurerm" {
 resource "azurerm_resource_group" "rg" {
   name     = "terraform-demo-${var.environment}"
   location = var.location
+
+}
+
+resource "azurerm_management_lock" "rg" {
+  name = "DoNotDelete"
+  scope = azurerm_resource_group.rg.id
+  lock_level = "CanNotDelete"
+  notes = "This Resource Group is Read-Only, please contact support if need further information !"
 }
