9045d069a6a160ceaf7c6e7925a72286affdba28
Chris Ayers <clayers@gmail.com>
2023-01-19 07:35:34 +0000

    updated workflows

--- terraform-on-azure/dev/main.tf
+++ terraform-on-azure/dev/main.tf
@@ -20,10 +20,3 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 
 }
-
-resource "azurerm_management_lock" "rg" {
-  name       = "DoNotDelete"
-  scope      = azurerm_resource_group.rg.id
-  lock_level = "CanNotDelete"
-  notes      = "This Resource Group is Read-Only, please contact support if need further information !"
-}
