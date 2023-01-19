8b730503bd275b9e3cdca9fd6032d5cc2d4849f2
Chris Ayers <clayers@gmail.com>
2023-01-19 00:13:33 +0000

    prod updates

--- terraform-on-azure/prod/main.tf
+++ terraform-on-azure/prod/main.tf
@@ -3,9 +3,10 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0.2"
+      version = "=3.7.0"
     }
   }
+  backend "azurerm" {}
 
   required_version = ">= 1.1.0"
 }
